9e6a77e8efe2a8e6d315d43f1f3f8523b8cb0333
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-23 19:55:11 +0200

    created Table component and pass props to map search/filter item

---
+++ src/Table.jsx
@@ -0,0 +1,22 @@
+function Table({ data }) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>E-mail</th>
+        </tr>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.first_name}</td>
+            <td>{item.last_name}</td>
+            <td>{item.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
