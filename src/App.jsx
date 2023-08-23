455d32a911b6d39f9404830c8b087fd6e676250c
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-23 19:30:21 +0200

    added basic html template and css class

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="app">
+      <input type="text" placeholder="Search" className="search" />
+      <ul className="list-items">
+        <li className="item">John</li>
+        <li className="item">Jane</li>
+        <li className="item">Merry</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
