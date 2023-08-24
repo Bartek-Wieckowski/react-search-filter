6275cc6d536c46174a7887674f0a389aff86087f
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-24 12:41:16 +0200

    added infinite scroll and auto loading data + 10 items, imit spinner, and added one column in table to better display fetch data during scroll

--- src/App.jsx
+++ src/App.jsx
@@ -52,40 +52,98 @@ import axios from 'axios';
 // }
 
 // 3) options with fetching
+// function App() {
+//   const [query, setQuery] = useState('');
+//   const [data, setData] = useState([]);
+//   const keys = ['first_name', 'last_name', 'email'];
+
+//   const searchAndFilter = (data) => {
+//     return data.filter((item) =>
+//       keys.some((key) => item[key].toLowerCase().includes(query))
+//     );
+//   };
+//   useEffect(() => {
+//     const fetchUsers = async () => {
+//       try {
+//         const res = await axios.get(`http://localhost:3004/users?q=${query}`);
+//         setData(res.data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchUsers();
+//   }, [query]);
+
+//   return (
+//     <div className="app">
+//       <input
+//         type="text"
+//         placeholder="Search"
+//         className="search"
+//         onChange={(e) => setQuery(e.target.value)}
+//         value={query}
+//       />
+//       <Table data={searchAndFilter(data)} />
+//     </div>
+//   );
+// }
+
+// 4) fetching and infinite scroll
 function App() {
-  const [query, setQuery] = useState('');
-  const [data, setData] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [visibleUsers, setVisibleUsers] = useState(10);
+  const [isLoading, setIsLoading] = useState(false);
+  const [shouldShowMore, setShouldShowMore] = useState(false);
+
   const keys = ['first_name', 'last_name', 'email'];
 
-  const searchAndFilter = (data) => {
-    return data.filter((item) =>
-      keys.some((key) => item[key].toLowerCase().includes(query))
-    );
-  };
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get(`http://localhost:3004/users?q=${query}`);
-        setData(res.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  const filteredUsers = Users.filter((item) =>
+    keys.some((key) => item[key].toLowerCase().includes(searchValue))
+  );
 
-    fetchUsers();
-  }, [query]);
+  const handleScroll = () => {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const contentHeight = document.body.offsetHeight;
+
+    if (scrollPosition >= contentHeight && !isLoading) {
+      setIsLoading(true);
+      setShouldShowMore(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (shouldShowMore) {
+      setTimeout(() => {
+        setVisibleUsers((prevVisibleUsers) => prevVisibleUsers + 10);
+        setIsLoading(false);
+        setShouldShowMore(false);
+      }, 1000);
+    }
+  }, [shouldShowMore]);
 
   return (
-    <div className="app">
-      <input
-        type="text"
-        placeholder="Search"
-        className="search"
-        onChange={(e) => setQuery(e.target.value)}
-        value={query}
-      />
-      <Table data={searchAndFilter(data)} />
-    </div>
+    <>
+      <div className="app" style={{ opacity: isLoading ? 0.5 : 1 }}>
+        <input
+          type="text"
+          placeholder="Search"
+          className="search"
+          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchValue}
+        />
+        {/* <Table data={searchAndFilter(Users)} /> */}
+        <Table data={filteredUsers.slice(0, visibleUsers)} />
+      </div>
+      {isLoading && <div className="spinner">Loading...</div>}
+    </>
   );
 }
 
--- src/Table.jsx
+++ src/Table.jsx
@@ -3,12 +3,14 @@ function Table({ data }) {
     <table>
       <tbody>
         <tr>
+          <th>No.</th>
           <th>First Name</th>
           <th>Last Name</th>
           <th>E-mail</th>
         </tr>
-        {data.map((item) => (
+        {data.map((item, i) => (
           <tr key={item.id}>
+            <td>{i + 1}</td>
             <td>{item.first_name}</td>
             <td>{item.last_name}</td>
             <td>{item.email}</td>
